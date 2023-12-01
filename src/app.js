9199878e393b17533974f1b7da41375ee574bd14
PauloGaldos2308 <innovati1.tr1.1@gmail.com>
2023-12-01 14:30:25 -0500

    confiuracion de apps para req.body
    configi¿ure el modulo de apps para que admita los datos req.body

--- src/app.js
+++ src/app.js
@@ -11,9 +11,14 @@ const indexRoutes = require('./routes/index.routes');
 const userRoutes = require('./routes/auth.routes');
 // Módulo de Node.js para trabajar con rutas de archivos
 const path = require('path');
+const bodyParser = require('body-parser');
 
 // Inicializaciones
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 // Configuraciones de la aplicación
 app.set('port', process.env.PORT || 3000);
